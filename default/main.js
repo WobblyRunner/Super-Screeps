491386b54be4dfcf3fb51ef819025f9c23305215
Joshua Willis <j.willis8@students.clark.edu>
2016-12-24 11:57:57 -0800

    Fixed a strange error

--- default/main.js
+++ default/main.js
@@ -1,19 +1,6 @@
 var roles = {
     harvester: require('role.harvester'),
     builder: require('role.builder'),
-    upgrader: require('role.upgrader')
-};
-
-var counts = {};
-
-module.exports.loop = function ( ) {
-    counts = {
-        harvester: 0,
-        builder: 0,
-        upgrader: 0
-    };var roles = {
-    harvester: require('role.harvester'),
-    builder: require('role.builder'),
     upgrader: require('role.upgrader'),
     repairer: require('role.repairer')
 };
@@ -57,22 +44,6 @@ module.exports.loop = function ( ) {
             roles.builder.create(spawn);
         } else if (quotas.upgrader > counts.upgrader) {
             roles.upgrader.create(spawn);
-        }
-    }
-}
-    
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        
-        if (creep.memory.role === 'harvester') {
-            roles.harvester.run(creep);
-            counts.harvester++;
-        } else if (creep.memory.role === 'builder') {
-            roles.builder.run(creep);
-            counts.builder++;
-        } else if (creep.memory.role === 'upgrader') {
-            roles.upgrader.run(creep);
-            counts.upgrader++;
         }
     }
 }
