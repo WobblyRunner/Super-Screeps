1288278b166d9c5aef3c6076355f2f6c1ce26641
Joshua Willis <j.willis8@students.clark.edu>
2016-12-24 11:55:39 -0800

    Automated spawning
    Added automated spawning.
    Counts variable: How many creeps there are at a given time.
    Quotas variable: How many creeps there should be.

--- default/main.js
+++ default/main.js
@@ -11,7 +11,55 @@ module.exports.loop = function ( ) {
         harvester: 0,
         builder: 0,
         upgrader: 0
+    };var roles = {
+    harvester: require('role.harvester'),
+    builder: require('role.builder'),
+    upgrader: require('role.upgrader'),
+    repairer: require('role.repairer')
+};
+
+var counts = {};
+var quotas = {
+    harvester: 1,
+    builder: 0,
+    upgrader: 1
+};
+var shouldSpawn = true;
+
+module.exports.loop = function ( ) {
+    counts = {
+        harvester: 0,
+        builder: 0,
+        upgrader: 0
     };
+    
+    var spawn = Game.spawns['Spawn1'];
+    
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        
+        if (creep.memory.role === 'harvester') {
+            roles.harvester.run(creep);
+            counts.harvester++;
+        } else if (creep.memory.role === 'builder') {
+            roles.builder.run(creep);
+            counts.builder++;
+        } else if (creep.memory.role === 'upgrader') {
+            roles.upgrader.run(creep);
+            counts.upgrader++;
+        }
+    }
+    
+    if (!spawn.spawning && shouldSpawn) {
+        if (quotas.harvester > counts.harvester) {
+            roles.harvester.create(spawn);
+        } else if (quotas.builder > counts.builder) {
+            roles.builder.create(spawn);
+        } else if (quotas.upgrader > counts.upgrader) {
+            roles.upgrader.create(spawn);
+        }
+    }
+}
     
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
